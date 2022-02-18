b7bda23ac9c5d5b304fdc81ca99ffee23e4e528a
Vikram Narra <vchandra.narra@gmail.com>
2022-02-17 21:50:09 -0500

    bruh

---
+++ Liger.js
@@ -0,0 +1,23 @@
+//Ligerr
+//Created 2022 Feb 16
+ 
+ 
+function display(board) {
+    // Design Board
+ 
+    // Design Board Pieces
+ 
+    // Develop the connection between the
+    // 2d mapping of the pieces to the board
+ 
+    // Visually display Forsyth-Edwards Notation
+ 
+    // *** This function will not return anything,
+    // it will be a passive function that will only print
+    // the current status of the board
+   
+ 
+}
+ 
+ 
+//
